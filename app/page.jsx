c7ad2a243d25919e233fdd69cfc88d1d072dee0e
Rotimi Shaibu <shaiburotimi485@gmail.com>
2025-11-15 22:18:35 +0100

    updating the pages.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,27 @@
-import { React } from "react";
+
+import Input from "@/components/assets/input.jsx";
 import "@/styles/globals.css";
 
 export default function LandingPage() {
   return (
-    <div className="landing-container">
-      <section>
-        <Input />
-      </section>
-      <section>
-        <h1>Welcome to the Voting Application</h1>
-        <p>Please navigate to the voting section to participate.</p>
-      </section>
+    <div className="main-container">
+      {/* background image will go in here..... */}
+      <div className="section-1">
+        <section>
+          <Input />
+        </section>
+      </div>
+
+      <div className="section-2">
+        {/* some voter-action images will fit in here..... */}
+        <section>
+          <h1>Welcome to the Voting Application</h1>
+          <p>Please navigate to the voting section to participate.</p>
+        </section>
+
+      </div>
+      
+      
       
     </div>
   )
